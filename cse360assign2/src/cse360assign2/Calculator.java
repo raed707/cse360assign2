865113674011727ca6fdd8f751af53a0b1918395
Raed <raed707@msn.com>
2019-02-25 20:06:05 -0700

    initial version

---
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,46 @@
+/**
+ * @author Raed Alghamdi
+ * Class ID: 311
+ * Assignment #2
+ * Description: Calculator.java contains methods that does mathematical operations. 
+ */
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/**
+	 * 
+	 * @param value is the integer inserted by the user
+	 */
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public void multiply (int value) {
+		
+	}
+	
+	public void divide (int value) {
+		
+	}
+	
+	public String getHistory () {
+		return "";
+	}
+}
